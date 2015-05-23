2bbf3eddbb797040eb03345cbdccedcbfbc3a8c1
Graham Goudeau <grahamgoudeau@gmail.com>
2015-05-23 19:09:08 -0400

    fixed setup script

--- setup_term.sh
+++ setup_term.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 1 ];
+if [ $# -lt 1 ];
     then echo "Provide indentation style: 'py' or 'c'"
-else
-    python set_indent_style.py $1
+    echo "Optionally provide 'n' as second arg for no restart"
+elif [ $# -eq 1 ]; then
+    python ~/set_indent_style.py $1
     echo "Finished setting indent style..."
     echo "Restarting bash so .bashrc takes effect..."
     exec bash
+elif [ "$2" == "n" ]; then
+    python ~/set_indent_style.py $1
+    echo "Finished setting indent style..."
+else
+    echo "Args: [indent style] ['n' if no restart]"
 fi
 
